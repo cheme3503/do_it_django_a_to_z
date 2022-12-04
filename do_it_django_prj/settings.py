681bb0d13b617450663cf8d3c81f05d923476448
cheme3503 <cheme3507@gmail.com>
2022-12-04 19:01:49 +0100

    도커에 장고만 담기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p%_vi5u!ts!nfye(@-=g(#dkp7lx5&)9=7lv1eacrkq3jt-a3_'
 
+SECRET_KEY = os.environ.get('SECRET_KEY', 'p%_vi5u!ts!nfye(@-=g(#dkp7lx5&)9=7lv1eacrkq3jt-a3_')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', 1))
 
-ALLOWED_HOSTS = []
+if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+else:
+    ALLOWED_HOSTS = []
 
 
 # Application definition
