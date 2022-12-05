c6c4465f4f819dddd7ffdefc707b76cec90b04d3
cheme3503 <cheme3507@gmail.com>
2022-12-05 09:48:48 +0100

    Host_변경

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -26,10 +26,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'p%_vi5u!ts!nfye(@-=g(#dkp7lx5&)9=7lv1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', 1))
 
-if os.environ.get('DJANGO_ALLOWED_HOSTS'):
-    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
-else:
-    ALLOWED_HOSTS = []
+# if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+#     ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+# else:
+ALLOWED_HOSTS = ['3.64.49.251',]
 
 
 # Application definition
