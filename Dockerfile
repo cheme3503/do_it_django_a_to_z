eb5ac9d335f1107ee2dfe720dcdeca2dd32f2bb9
cheme3503 <cheme3507@gmail.com>
2022-12-20 12:39:13 +0100

    Nginx 적용하기

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ ENV PYTHONUNBUFFERED 1
 
 RUN apk update
 RUN apk add postgresql-dev gcc python3-dev musl-dev zlib-dev jpeg-dev #--(5.2)
+RUN apk add libffi-dev
 
 COPY . /usr/src/app/
 # install dependencies
--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'do_it_django_prj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'ENGINE': os.environ.get("SQL_ENGINE", 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.environ.get('SQL_USER', 'user'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
+        'HOST': os.environ.get('SQL_HOST', 'localhost'),
+        'PORT': os.environ.get("SQL_PORT", '5432'),
 
     }
 }
@@ -131,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'_static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
